656d61f63a4b5d33fbfdcd1cde0a0f3c894d5a01
Seong Jin Kim <seongjinkimsjk@gmail.com>
2021-08-25 19:47:41 -0400

    Board component initial styling

--- src/components/Tetris/Board.js
+++ src/components/Tetris/Board.js
@@ -4,7 +4,17 @@ function Board(props) {
   const gameBoard = props.createBoard;
 
   return (
-    <div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: `repeat(${gameBoard.length},calc(25vw / ${gameBoard[0].length}))`,
+        gridTemplateColumns: `repeat(${gameBoard[0].length}, 1fr)`,
+        gridGap: "1px",
+        border: "2px solid #333",
+        width: "100%",
+        maxWidth: "25vw",
+      }}
+    >
       {gameBoard.map((row) =>
         row.map((cell, idx) => <Cell key={idx} type={cell[0]} />)
       )}
